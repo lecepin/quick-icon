e898cf703824de165007d6380d8ec8239d7eacec
lecepin <383810086@qq.com>
2023-12-01 21:12:33 +0800

    feat: done

---
+++ README.md
@@ -0,0 +1,134 @@
+## 一键 SVG 文件转 React 组件
+
+```
+npm i -g qicon
+```
+
+配置文件默认读取 `.qicon.js`，可通过 `qicon -c xxx.js` 指定。
+
+配置文件格式：
+
+```js
+module.exports = {
+  // 有色 svg 列表
+  colorIcon: [
+    {
+      // 名称
+      name: "goal",
+      // svg 内容
+      content: `<?xml version="1.0" encoding="UTF-8"?>`,
+    },
+  ],
+  // 单色 svg 列表
+  solidColorIcon: [
+    {
+      name: "lock",
+      content: `<svg width="16" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M0 0h16v16H0z"/><path d="M10.5 6.3v-1a2.5 2.5 0 0 0-5 0v1h5zm1.5 1H4v6h8v-6zM9.75 9.8a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-3.5a.25.25 0 0 1-.25-.25v-.5a.25.25 0 0 1 .25-.25zM4.5 5.3a3.5 3.5 0 0 1 7 0v1h.5a1 1 0 0 1 1 1v6a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1v-6a1 1 0 0 1 1-1h.5v-1z" fill="#0D5DFF" class="fill-line"/></g></svg>`,
+    },
+  ],
+  // 从文件夹读取彩色 svg 文件
+  colorIconDirPath: "./",
+  // 从文件夹读取单色 svg 文件
+  solidColorIconDirPath: "./",
+
+  // React 组件 TSX 导出目录
+  output: "./dist",
+
+  // React 组件前缀
+  compoentPrefix: "Icon",
+};
+```
+
+在配置文件列表中的 svg 会和从文件读取的 svg 取并集。
+
+生成效果：
+
+index.tsx
+
+```tsx
+import React, { FC } from "react";
+
+type TProps = {
+  className?: string;
+  style?: React.CSSProperties;
+};
+
+export const Icongoal: FC<TProps> = ({ className, style = {} }) => (
+  <div
+    style={{
+      background:
+        'url("data:image/svg+xml,%3Csvg...%3E%3C%2Fsvg%3E") no-repeat',
+      backgroundSize: "100% 100%",
+      backgroundColor: "transparent",
+      width: "1em",
+      height: "1em",
+      ...style,
+    }}
+    className={className}
+  />
+);
+
+export const Iconlocak: FC<TProps> = ({ className, style = {} }) => (
+  <div
+    style={{
+      mask: 'url("data:image/svg+xml,%3Csvg%...3C%2Fsvg%3E") no-repeat',
+      WebkitMask: 'url("data:image/svg+xml,%3Csvg%...vg%3E") no-repeat',
+      backgroundColor: "currentColor",
+      WebkitMaskSize: "100% 100%",
+      maskSize: "100% 100%",
+      width: "1em",
+      height: "1em",
+      ...style,
+    }}
+    className={className}
+  />
+);
+```
+
+直接导入 `index.tsx` 即可以消费图标，纯色图标可通过 `color` CSS 属性进行改色。
+
+执行效果：
+
+```bash
+$ qicon
+
+⏳ ===> 查找单色图标
+---> 📄 找到单色图标: bread-delimiter.svg
+---> 📄 找到单色图标: icon-copy.svg
+---> 📄 找到单色图标: icon-delete.svg
+---> 📄 找到单色图标: icon-edit.svg
+---> 📄 找到单色图标: icon-lock.svg
+---> 📄 找到单色图标: icon-one.svg
+---> 📄 找到单色图标: info.svg
+---> 📄 找到单色图标: min.svg
+---> 📄 找到单色图标: send.svg
+---> 📄 找到单色图标: terminal-icon-all.svg
+---> 📄 找到单色图标: terminal-icon-folder.svg
+---> 📄 找到单色图标: 运营.svg
+
+⏳ ===> 生成颜色图标
+---> ✅ 生成成功: goal
+
+⏳ ===> 生成单色图标
+---> ✅ 生成成功: locak
+---> ✅ 生成成功: bread-delimiter
+---> ✅ 生成成功: icon-copy
+---> ✅ 生成成功: icon-delete
+---> ✅ 生成成功: icon-edit
+---> ✅ 生成成功: icon-lock
+---> ✅ 生成成功: icon-one
+---> ✅ 生成成功: info
+---> ✅ 生成成功: min
+---> ✅ 生成成功: send
+---> ✅ 生成成功: terminal-icon-all
+---> ✅ 生成成功: terminal-icon-folder
+---> ✅ 生成成功: 运营
+
+⏳ ===> 生成组件文件 index.tsx
+---> ✅ 生成成功: index.tsx
+
+⏳ ===> 生成预览文件 preview.html
+---> ✅ 生成成功: preview.html
+
+ 🎉 执行完成 🎉
+```
---
+++ main.js
@@ -0,0 +1,265 @@
+const { optimize } = require("svgo");
+const fs = require("fs");
+const path = require("path");
+
+const cIndex = process.argv.indexOf("-c");
+const configFileName =
+  cIndex === -1 || cIndex === process.argv.length - 1
+    ? "./.qicon.js"
+    : process.argv[process.argv.indexOf("-c") + 1];
+
+const duplicateNames = [];
+let config = {};
+let tsStr = `import React, { FC } from 'react'
+
+type TProps = {
+  className?: string
+  style?: React.CSSProperties
+}
+
+`;
+let previewStr = "";
+let previewStrIndex = 0;
+
+try {
+  config = require(`./${configFileName}`);
+  config.output = config.output || "./icons";
+} catch (error) {
+  console.error("🚫 无法读取配置文件：", error);
+  process.exit(1);
+}
+
+if (fs.existsSync(config.colorIconDirPath)) {
+  console.log("⏳ ===> 查找颜色图标");
+  const files = fs.readdirSync(config.colorIconDirPath);
+  files.forEach((file) => {
+    if (file.endsWith(".svg")) {
+      console.log(`---> 📊 找到颜色图标: ${file}`);
+      const content = fs.readFileSync(
+        path.resolve(config.colorIconDirPath, file),
+        "utf-8"
+      );
+      config.colorIcon.push({
+        name: file.replace(".svg", ""),
+        content,
+      });
+    }
+  });
+}
+
+if (fs.existsSync(config.solidColorIconDirPath)) {
+  console.log("\n⏳ ===> 查找单色图标");
+  const files = fs.readdirSync(config.solidColorIconDirPath);
+  files.forEach((file) => {
+    if (file.endsWith(".svg")) {
+      console.log(`---> 📄 找到单色图标: ${file}`);
+      const content = fs.readFileSync(
+        path.resolve(config.solidColorIconDirPath, file),
+        "utf-8"
+      );
+      config.solidColorIcon.push({
+        name: file.replace(".svg", ""),
+        content,
+      });
+    }
+  });
+}
+
+config.solidColorIcon.forEach((item) => {
+  if (config.colorIcon.some((obj) => obj.name === item.name)) {
+    duplicateNames.push(item.name);
+  }
+});
+
+if (duplicateNames.length > 0) {
+  console.error(`\n🚫 以下图标重名：${duplicateNames.join(", ")}`);
+  process.exit(1);
+}
+
+if (!fs.existsSync(config.output)) {
+  fs.mkdirSync(config.output, { recursive: true });
+}
+
+console.log("\n⏳ ===> 生成颜色图标");
+config.colorIcon.forEach((item) => {
+  try {
+    const result = optimize(item.content, {
+      plugins: [
+        { name: "removeAttrs", params: { attrs: "class" } },
+        {
+          name: "preset-default",
+        },
+      ],
+    });
+
+    tsStr += `export const ${
+      config.compoentPrefix +
+      item.name.replace(/-([a-z])/g, (_, letter) => letter.toUpperCase())
+    }: FC<TProps> = ({ className, style = {} }) => (
+  <div
+    style={{
+      background: 'url("data:image/svg+xml,${encodeURIComponent(
+        result.data
+      )}") no-repeat',
+      backgroundSize: '100% 100%',
+      backgroundColor: 'transparent',
+      width: '1em',
+      height: '1em',
+      ...style,
+    }}
+    className={className}
+  />
+);
+
+`;
+    previewStr += `<div class="a"><div class="icon"
+style="background: url('data:image/svg+xml,${encodeURIComponent(
+      result.data
+    )}') no-repeat;
+  background-size: 100% 100%;
+  background-color: transparent;
+  width: 1em;
+  height: 1em;"
+></div>${++previewStrIndex}.${item.name}</div>`;
+
+    console.log(`---> ✅ 生成成功: ${item.name}`);
+  } catch (error) {
+    console.log(`---> ❌ 生成失败: ${item.name}`);
+  }
+});
+
+console.log("\n⏳ ===> 生成单色图标");
+config.solidColorIcon.forEach((item) => {
+  try {
+    const result = optimize(item.content, {
+      plugins: [
+        { name: "removeAttrs", params: { attrs: "class" } },
+        {
+          name: "preset-default",
+        },
+        {
+          name: "fill-currentColor",
+          fn: () => {
+            return {
+              element: {
+                enter: (node) => {
+                  if (
+                    node.attributes.fill == null ||
+                    node.attributes.fill == "" ||
+                    node.attributes.fill == "none"
+                  ) {
+                    return;
+                  }
+
+                  node.attributes.fill = "currentColor";
+                },
+              },
+            };
+          },
+        },
+      ],
+    });
+
+    tsStr += `export const ${
+      config.compoentPrefix +
+      item.name.replace(/-([a-z])/g, (match, letter) => letter.toUpperCase())
+    }: FC<TProps> = ({ className, style = {} }) => (
+  <div
+    style={{
+      mask: 'url("data:image/svg+xml,${encodeURIComponent(
+        result.data
+      )}") no-repeat',
+      WebkitMask: 'url("data:image/svg+xml,${encodeURIComponent(
+        result.data
+      )}") no-repeat',
+      backgroundColor: 'currentColor',
+      WebkitMaskSize: '100% 100%',
+      maskSize: '100% 100%',
+      width: '1em',
+      height: '1em',
+      ...style,
+    }}
+    className={className}
+  />
+);
+
+`;
+    previewStr += `<div class="a"><div class="icon"
+    style="mask: url('data:image/svg+xml,${encodeURIComponent(
+      result.data
+    )}') no-repeat;
+      -webkit-mask: url('data:image/svg+xml,${encodeURIComponent(
+        result.data
+      )}') no-repeat;
+      background-color: currentColor;
+      -webkit-mask-size: 100% 100%;
+      mask-size: 100% 100%;
+      width: 1em;
+      height: 1em;" 
+  ></div>${++previewStrIndex}.${item.name}</div>`;
+    console.log(`---> ✅ 生成成功: ${item.name}`);
+  } catch (error) {
+    console.log(`---> ❌ 生成失败: ${item.name}`);
+  }
+});
+
+console.log("\n⏳ ===> 生成组件文件 index.tsx");
+try {
+  fs.writeFileSync(path.resolve(config.output, "index.tsx"), tsStr);
+  console.log("---> ✅ 生成成功: index.tsx");
+} catch (error) {
+  console.log("---> ❌ 生成失败: index.tsx");
+}
+
+console.log("\n⏳ ===> 生成预览文件 preview.html");
+try {
+  fs.writeFileSync(
+    path.resolve(config.output, "preview.html"),
+    `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Icon Preview</title>
+  <style>
+    .icon {
+      display: inline-block;
+      width: 2em!important;
+      height: 2em!important;
+      margin: 1em;
+      transition: all .3s;
+    }
+    .a:hover .icon{
+      filter: drop-shadow(0 0 2px rgba(0, 0, 0, .5));
+      transform: scale(1.5);
+      transition: all .3s;
+      color: pink;
+    }
+    .a {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+      margin: 1em;
+      border: 1px solid #ccc;
+    }
+    body{
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+      align-items: center;
+      background-color: #f5f5f5;
+      padding: 2em;
+    }
+  </style>
+</head>
+<body>
+  ${previewStr}
+</body>
+</html>`
+  );
+  console.log("---> ✅ 生成成功: preview.html");
+} catch (error) {
+  console.log("---> ❌ 生成失败: preview.html");
+}
+
+console.log("\n 🎉 执行完成 🎉");
